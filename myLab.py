1651a38d21f7a430d44a96c42f621cdcba857c44
damnpav <32949863+damnpav@users.noreply.github.com>
2019-10-15 20:34:26 +0300

    some changes

--- myLab.py
+++ myLab.py
@@ -4,6 +4,7 @@ Created on Tue Jun 18 15:12:45 2019
 
 @author: dpavlin001
 """
+# TODO make MySQL store for shares
 
 sp500base = 'C:/Users/hcani/Documents/GitHub/myLab/sp500.xlsx' #путь к файлу с Сп500
 dj = 'C:/Users/hcani/Documents/GitHub/myLab/dowJones.xlsx'
@@ -12,7 +13,7 @@ rts = 'C:/Users/hcani/Documents/GitHub/myLab/rts.xlsx'
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def indComp(rout): #retrieve data from excel frames
+def indComp(rout): # retrieve data from excel frames
     myDf = pd.read_excel(rout, "comp")
     compNames = myDf['Company'].tolist()
     compSymbols = myDf['Symbol']
@@ -29,15 +30,25 @@ def showStructure(indexLists, porog): #show structure of index
     plt.pie(sizes, labels = labels)
     plt.axis('equal')
     plt.show()
-    
-#spList = indComp(sp500base)
-#djList = indComp(dj)
-#nasdaqList = indComp(nasdaq)
 
-rtsList = indComp(rts)
-showStructure(rtsList, 0.01)
+def builtGraph:
 
 
-#showStructure(spList, 1.4)
-#showStructure(djList, 1.4)
+# spList = indComp(sp500base)
+# djList = indComp(dj)
+# nasdaqList = indComp(nasdaq)
+
+# rtsList = indComp(rts)
+# showStructure(rtsList, 0.01)
+
+
+# showStructure(spList, 1.4)
+# showStructure(djList, 1.4)
 #showStructure(nasdaqList, 1.4)
+
+copyDates = []
+for i in range(len(dates)):
+    if i % delimiter != 0:
+        copyDates.append('')
+    else:
+        copyDates.append(dates[i])
