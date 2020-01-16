8ca4f3e25e9e14cd3eb6d8f6ed62779135112e0f
damnpav <32949863+damnpav@users.noreply.github.com>
2020-01-17 01:59:40 +0300

    df from sql

--- draft.py
+++ draft.py
@@ -5,7 +5,21 @@ Created on Mon Jul 29 03:40:24 2019
 @author: hcani
 """
 
-import pyodbc
+import mysql.connector
+import pandas as pd
 
-connection = pyodbc.connect(DRIVER={Devart ODBC Driver for MySQL};User ID=root;Password=ADMIN12345; \
-Server=MySQL;Database=stocks;Port=33060;String Types=Unicode)
+pathToPass = r"C:\Users\hcani\Documents\MySQL Settings\sql_key.txt"
+password = open(pathToPass).readlines()[0]
+
+cnx = mysql.connector.connect(user = 'root', password = password, host = '127.0.0.1', database = 'stocks')
+cursor = cnx.cursor()
+
+query = ("SELECT * FROM ibexdata WHERE id = 0")
+#cursor.execute(query)
+# data = cursor.fetchall()
+# print(data)
+
+df = pd.read_sql('SELECT * FROM ibexdata', con = cnx)
+print(df)
+
+cnx.close()
--- functionsPool.py
+++ functionsPool.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import plotly.graph_objects as go
-import pyodbc
+import seaborn as sns
+import mysql.connector
 
-# TODO wrap it in function
-df = pd.read_csv(r'C:\Users\hcani\Documents\GitHub\myLab\marketData\ANA.MC.csv')  # your df
 
-def grapher(yourTitle, xTitle, yTitle, dataFrame):
+#  df = pd.read_csv(r'C:\Users\hcani\Documents\GitHub\myLab\marketData\ANA.MC.csv')  # your df
+# TODO debug candles
+# think about figures and seaborn objectes for GUI
+def candles(yourTitle, xTitle, yTitle, dataFrame):
     """
     Function for building market figures, returning figure object
     :param yourTitle: Title of graph
@@ -28,6 +30,23 @@ def grapher(yourTitle, xTitle, yTitle, dataFrame):
                       xaxis_rangeslider_visible=True, xaxis_title= xTitle, yaxis_title= yTitle)
     return fig
 
-# grapher('AMA.MC', 'Date', 'Price', df).show()
+# TODO optimisate that crap
+def tics(linewidth, dataFrame, stockName):
+    sns.set(style = "whitegrid")
+    data = pd.DataFrame(list(dataFrame['Close'].values), list(dataFrame['Date'].values), columns = [stockName])
+    sns.lineplot(data = data, palette = 'tab10', linewidth = linewidth)
 
-# write a function for selecting data from MySQL to dataframe from showing praphs
+pathToPass = r"C:\Users\hcani\Documents\MySQL Settings\sql_key.txt"
+password = open(pathToPass).readlines()[0]
+def getDf(stockId, password):
+    cnx = mysql.connector.connect(user='root', password=password, host='127.0.0.1', database='stocks')
+    cursor = cnx.cursor()
+    query = ("SELECT * FROM ibexdata WHERE id = " + str(stockId))
+    df = pd.read_sql(query, con = cnx)
+    cnx.close()
+    return df
+
+df = getDf(0, password)
+print(1)
+fig = candles('AMC', 'Date', 'Price', df)
+fig.show()
