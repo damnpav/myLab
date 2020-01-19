447c00328a6726e14141f9777be3b7ba281f9dc5
damnpav <32949863+damnpav@users.noreply.github.com>
2020-01-19 23:41:07 +0300

    tics function

--- functionsPool.py
+++ functionsPool.py
@@ -5,7 +5,7 @@ import mysql.connector
 
 
 #  df = pd.read_csv(r'C:\Users\hcani\Documents\GitHub\myLab\marketData\ANA.MC.csv')  # your df
-# TODO debug candles
+# TODO make from date to date
 # think about figures and seaborn objectes for GUI
 def candles(yourTitle, xTitle, yTitle, dataFrame):
     """
@@ -30,23 +30,33 @@ def candles(yourTitle, xTitle, yTitle, dataFrame):
                       xaxis_rangeslider_visible=True, xaxis_title= xTitle, yaxis_title= yTitle)
     return fig
 
-# TODO optimisate that crap
-def tics(linewidth, dataFrame, stockName):
-    sns.set(style = "whitegrid")
-    data = pd.DataFrame(list(dataFrame['Close'].values), list(dataFrame['Date'].values), columns = [stockName])
-    sns.lineplot(data = data, palette = 'tab10', linewidth = linewidth)
+# TODO make from date to date
+# TODO lambda function
+def tics(dataFrame, stockName, adjCoeff):
+    sns.set(style="whitegrid")
+    x_labels = []
+    for i in range(len(list(dataFrame['Date'].values))):
+        if i%adjCoeff == 0:
+            x_labels.append(list(dataFrame['Date'].values)[i])
+        else:
+            x_labels.append("")
+    data = pd.DataFrame(list(dataFrame['Close'].values), x_labels, columns=[stockName])
+    g = sns.lineplot(data=pd.to_numeric(data[stockName]))
+    g.set_xticklabels(rotation=45)
+
 
 pathToPass = r"C:\Users\hcani\Documents\MySQL Settings\sql_key.txt"
 password = open(pathToPass).readlines()[0]
 def getDf(stockId, password):
     cnx = mysql.connector.connect(user='root', password=password, host='127.0.0.1', database='stocks')
-    cursor = cnx.cursor()
     query = ("SELECT * FROM ibexdata WHERE id = " + str(stockId))
     df = pd.read_sql(query, con = cnx)
     cnx.close()
     return df
 
 df = getDf(0, password)
-print(1)
-fig = candles('AMC', 'Date', 'Price', df)
-fig.show()
+# print(1)
+# fig = candles('AMC', 'Date', 'Price', df)
+# fig.show()
+
+tics(0.2, df, 'AMC')
