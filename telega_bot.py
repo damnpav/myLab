bc1a15715ee1a04d073426df67eeb08fd6a5e986
damnpav <32949863+damnpav@users.noreply.github.com>
2020-11-04 22:21:16 +0300

    add new features

--- telega_bot.py
+++ telega_bot.py
@@ -5,6 +5,12 @@ import os
 import re
 from datetime import datetime
 import traceback
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import models
+
 
 bot_token = open('/home/jet/Документы/telega_api.txt').readline()[:-1]
 test_chat_id = '305402034'
@@ -18,16 +24,38 @@ def cor_calc(tickers_str):
                "ticker - use ticker searcher."
     from models import correlation_calc as cc
     return cc(tickers_list).to_string()
+    # how to send
+    #my_bot.send_photo(chat_id=test_chat_id, photo=foto, caption='Portf_picture', parse_mode='HTML')
 
 
 def welcoming_buttons():
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton(text='Model portfolio', callback_data='model_cb'),
-               types.InlineKeyboardButton(text='Correlation radar', callback_data='corr_calc_cb'),
-               types.InlineKeyboardButton(text='Ticker searcher', callback_data='searcher_cb'),
-               types.InlineKeyboardButton(text='About project', callback_data='about'),
+    markup.add(types.InlineKeyboardButton(text='Portfolio', callback_data='model_cb'),
+               types.InlineKeyboardButton(text='Correlations', callback_data='corr_calc_cb'),
+               types.InlineKeyboardButton(text='Find ticker', callback_data='searcher_cb'),
+               types.InlineKeyboardButton(text='About', callback_data='about'),
                types.InlineKeyboardButton(text='Contact us', callback_data='contact'))
     return markup
+
+
+def print_out_model(shares, quantity):
+    for share in shares:
+        if not models.check_ticker(share):
+            return f'{share} ticker not found at our base. Temporally we use only tickers from SPB Stock Exchange.' \
+                   f'You may search for ticker of your company with /search command.'
+    portf = models.Portfolio(shares, quantity, [1]*len(shares), [1]*len(shares))
+    return_str = f'Expectation return: {round(portf.portfolio_exp_ret*253*100, 2)} % annually.' \
+                 f'Risk: {round(portf.general_variance*253*100, 2)} % annually.' \
+                 f'Risk\ Reward ratio: {round(portf.general_variance/portf.portfolio_exp_ret, 2)}'
+
+
+def weights_pie(weights, pie_name):
+    labels = list(weights.keys())
+    sizes = list(weights.values())
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.savefig(pie_name)
 
 
 class Session:
@@ -49,6 +77,7 @@ class Session:
 
 
 def main(bot_token):
+    get_message = False  # flag for controlling loop time for using and non-using period
     new_session = Session(bot_token)
     for el in new_session.received_list:
         another_dict = eval(str(el).replace('<', '"').replace('>', '"'))  # parse collected messages to dict
@@ -57,41 +86,67 @@ def main(bot_token):
         if another_dict['edited_message'] is not None:
             continue
 
+        update_id = str(el.update_id)
+        # avoid answered messages
+        if update_id in new_session.replied_list:
+            continue
+
         if el.callback_query:
             message_id = str(el.callback_query.message.json['message_id'])
             message_text = str(el.callback_query.data)
             your_chat_id = str(el.callback_query.message.chat.id)
+            username = str(el.callback_query.from_user.username)
         else:
             message_id = str(another_dict['message']['json']['message_id'])
             message_text = str(another_dict['message']['text'])
             your_chat_id = str(another_dict['message']['json']['chat']['id'])
+            username = str(el.message.from_user.username)
 
-        # avoid answered messages
-        if message_id in new_session.replied_list:
-            continue
+        get_message = True  # flag for controlling loop time for using and non-using period
 
-        if re.search(r'start', message_text):
-            new_session.send_buttons('Welcome to PavlinLab! \n Choose your option:', your_chat_id, welcoming_buttons())
-        elif re.search(r'model_cb', message_text):
-            new_session.send_message('For creating model of your portfolio please type command \n//model and write '
+        if re.search(r'model_cb', message_text):
+            new_session.send_message('For creating model of your portfolio please type command \n/model and write '
                                      'tickers of shares that you want to include in your portfolio, sep by comma. '
-                                     '\n\nFor example:\n //model AMD, AMZN, MSFT, BK, AAPL\n\n'
-                                     'You may search for ticker of your company with //search command.', your_chat_id)
+                                     '\n\nFor example:\n/model AMD, AMZN, MSFT, BK, AAPL\n\n'
+                                     'You may search for ticker of your company with /search command.', your_chat_id)
+        elif re.search(r'corr_calc_cb', message_text):
+            new_session.send_message('To count correlation between some shares please type command \n/correlation '
+                                     'and write tickers of these papers, sep by comma.'
+                                     '\n\nFor example: \n/correlation INTC, CSCO, FB\n\n'
+                                     'You may search for ticker of your company with /search command.', your_chat_id)
+        elif re.search(r'searcher_cb', message_text):
+            new_session.send_message('You may search for ticker of your company with /search command.\n\n'
+                                     'For example: \n/search Xerox', your_chat_id)
+        elif re.search(r'about', message_text):
+            new_session.send_message('PavlinLab Project is a tool for modelling portfolio of securities '
+                                     'with Modern Portfolio Theory.', your_chat_id)
+        elif re.search(r'contact', message_text):
+            new_session.send_message('You may always contact us on e-mail: hcanilvap@gmail.com', your_chat_id)
+        else:
+            new_session.send_buttons('Welcome to PavlinLab! \n Choose your option:', your_chat_id, welcoming_buttons())
+
         with open(path_to_replied_id, 'a') as replied_id_file:
-            replied_id_file.write(message_id + '\n')  # save id of aswered message
+            replied_id_file.write(update_id + '\n')  # save id of answered message
+        with open('log.txt', 'a') as log_file:
+            log_file.write(f'Username: {username}, Message_id: {message_id}, Message_text: {message_text}, '
+                           f'Chat_id: {your_chat_id}, time: {datetime.now().strftime("%Y-%m-%d %H:%M")} \n\n')
     del new_session  # clear class
+    return get_message
 
 
 while not os.path.isfile('/home/jet/Документы/myLab/bot_stop.txt'):
+    t = 15
     try:
-        main(bot_token)
+        answer_flag = main(bot_token)
+        if answer_flag:
+            t = 5   # reducing answer time when bot have conversation
     except Exception as e:
         print(traceback.format_exc())
         with open('log.txt', 'a') as log_file:
             log_file.write(str(e) + ': ' + str(traceback.format_exc()) + ' - ' +
                            datetime.now().strftime("%Y-%m-%d %H:%M") + '\n\n')
         pass
-    time.sleep(10)
+    time.sleep(t)
 
 
 #main(bot_token)
