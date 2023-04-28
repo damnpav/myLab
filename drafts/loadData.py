a8b64d5324cd45dc4947c12839726d7f43e68d7c
damnpav <hcanilvap@gmail.com>
2023-04-28 15:19:28 +0300

    refactor of old files to folders

--- drafts/loadData.py
+++ drafts/loadData.py
@@ -3,7 +3,7 @@ import yfinance as yf
 from sqlalchemy import create_engine
 rtsi_df = pd.read_excel('rtsi_filled.xlsx')
 
-engine = create_engine('mysql+pymysql://root:Slim171072Shady!@localhost:3306/indexes')
+engine = create_engine('mysql+pymysql://root:!@localhost:3306/indexes')
 conn = engine.connect()
 
 #conn.execute("INSERT INTO RTSI_keys VALUES (0, 'APPLE', N'ЯБЛОКО', N'Яблочная компания')")
